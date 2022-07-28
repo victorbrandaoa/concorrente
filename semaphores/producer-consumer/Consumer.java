d328716f33a10240b74fe8c60ca1acd476685571
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2022-07-27 23:07:26 -0300

    add some documentation to the producer-consumer code

--- semaphores/producer-consumer/Consumer.java
+++ semaphores/producer-consumer/Consumer.java
@@ -19,18 +19,18 @@ public class Consumer extends Thread {
     @Override
     public void run() {
         try {
-            consSem.acquire();
-            mutex.acquire();
-            int v = buffer.get(cIn);
-            cIn = (cIn + 1) % buffer.getSize();
+            consSem.acquire(); // consSem.down() - if the buffer is empty, wait until some producer adds an item
+            mutex.acquire(); // mutex.down()
+            int v = buffer.get(cIn); // get the value from the buffer
+            cIn = (cIn + 1) % buffer.getSize(); // update the index where the next consumer must get the value to process
             String msg = String.format(
                     "Consumer %s has finished the value %d",
                     Thread.currentThread().getName(),
                     v
             );
             System.out.println(msg);
-            mutex.release();
-            prodSem.release();
+            mutex.release(); // mutex.up()
+            prodSem.release(); // prodSem.up() - notify the producers threads that there is an empty space in the buffer to add a new item
         }  catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
--- semaphores/producer-consumer/Producer.java
+++ semaphores/producer-consumer/Producer.java
@@ -21,18 +21,18 @@ public class Producer extends Thread {
     @Override
     public void run() {
         try {
-            prodSem.acquire();
-            mutex.acquire();
-            buffer.put(pIn, this.v);
-            pIn = (pIn + 1) % buffer.getSize();
+            prodSem.acquire(); // prodSem.down() - if the buffer is full, wait until some consumer removes an item
+            mutex.acquire(); // mutex.down()
+            buffer.put(pIn, this.v); // add the produced value to the buffer
+            pIn = (pIn + 1) % buffer.getSize(); // update the index where the next producer must add its produced value
             String msg = String.format(
                     "Producer %s has finished the value %d",
                     Thread.currentThread().getName(),
                     this.v
             );
             System.out.println(msg);
-            mutex.release();
-            consSem.release();
+            mutex.release(); // mutex.up()
+            consSem.release(); // consSem.up() - notify the consumers threads that there is a new item to process
         }  catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
